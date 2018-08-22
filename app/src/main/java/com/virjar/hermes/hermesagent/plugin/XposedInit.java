987ea6c230d8b046b4391fcbac89ad39dc0c2418
virjar <weijia.deng@qunar.com>
2018-08-22 22:09:55 +0800

    init project

---
+++ app/src/main/java/com/virjar/hermes/hermesagent/plugin/XposedInit.java
@@ -0,0 +1,15 @@
+package com.virjar.hermes.hermesagent.plugin;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * Created by virjar on 2018/8/22.
+ */
+
+public class XposedInit implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+
+    }
+}
