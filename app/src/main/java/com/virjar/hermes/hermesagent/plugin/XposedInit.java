7617c356f9f08a8c440079def939e03f0bbe3877
virjar <weijia.deng@qunar.com>
2018-09-14 17:57:56 +0800

    warning for hot load failed

--- app/src/main/java/com/virjar/hermes/hermesagent/plugin/XposedInit.java
+++ app/src/main/java/com/virjar/hermes/hermesagent/plugin/XposedInit.java
@@ -2,12 +2,15 @@ package com.virjar.hermes.hermesagent.plugin;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 
 import com.google.common.collect.Maps;
+import com.virjar.hermes.hermesagent.R;
 import com.virjar.hermes.hermesagent.util.Constant;
 
 import org.apache.commons.lang3.StringUtils;
@@ -35,15 +38,15 @@ public class XposedInit implements IXposedHookLoadPackage {
         if (StringUtils.equalsIgnoreCase(lpparam.packageName, Constant.packageName)) {
             return;
         }
-        if (StringUtils.equalsIgnoreCase(lpparam.packageName, "de.robv.android.xposed.installer")) {
-            XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook(XCallback.PRIORITY_HIGHEST * 2) {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    fixXposedInstallerAppUpdate((Context) param.args[0], lpparam);
-                }
-            });
-
-        }
+//        if (StringUtils.equalsIgnoreCase(lpparam.packageName, "de.robv.android.xposed.installer")) {
+//            XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook(XCallback.PRIORITY_HIGHEST * 2) {
+//                @Override
+//                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//                    fixXposedInstallerAppUpdate((Context) param.args[0], lpparam);
+//                }
+//            });
+//
+//        }
         if (hooked) {
             return;
         }
@@ -78,24 +81,44 @@ public class XposedInit implements IXposedHookLoadPackage {
         Class<?> packageChangeReceiverClass = XposedHelpers.findClassIfExists("de.robv.android.xposed.installer.receivers.PackageChangeReceiver", lpparam.classLoader);
 
         if (packageChangeReceiverClass != null) {
-            //XposedHelpers.findAndHookMethod(packageChangeReceiverClass, "onReceive", Context.class, Intent.class, forceUpdateHook);
+            XposedHelpers.findAndHookMethod(packageChangeReceiverClass, "onReceive", Context.class, Intent.class, forceUpdateHook);
         }
         packageChangeReceiverClass = XposedHelpers.findClassIfExists("de.robv.android.xposed.installer.PackageChangeReceiver", lpparam.classLoader);
         if (packageChangeReceiverClass != null) {
-            // XposedHelpers.findAndHookMethod(packageChangeReceiverClass, "onReceive", Context.class, Intent.class, forceUpdateHook);
+            XposedHelpers.findAndHookMethod(packageChangeReceiverClass, "onReceive", Context.class, Intent.class, forceUpdateHook);
         }
         //TDOO
 
     }
 
+    private void alertHotLoadFailedWarning(Context context) {
+        new AlertDialog.Builder(context)
+                .setTitle("HermesAgent热发代码加载失败")
+                .setMessage("Xposed模块热加载失败，热发代码可能不生效，\n" +
+                        "有两个常见原因可能引发这个问题，请check:\n" +
+                        "1.您的Android studio开启了Instant Run，这会导致Xposed框架无法记载到正确的回调class" +
+                        "2.您安装了新代码之后，需要先打开一次HermesAgent的App，才能重启Android系统，否则Xposed会在init进程为HermesAgent的apk创建odex缓存。" +
+                        "  这会导致该文件创建者为root，进而再次热发代码的时候，普通进程没有remove老的odex文件缓存的权限，导致apk代码刷新失败 "
+                )
+                .setIcon(R.mipmap.ic_launcher)
+                .setNeutralButton("我已知晓！", new DialogInterface.OnClickListener() {//添加普通按钮
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .create().show();
+    }
+
     private void hotLoadPlugin(Context context, XC_LoadPackage.LoadPackageParam lpparam) {
         ClassLoader hotClassLoader = replaceClassloader(context, lpparam);
 
-        Class<?> aClass = null;
+        Class<?> aClass;
         try {
             aClass = hotClassLoader.loadClass(Constant.xposedHotloadEntry);
         } catch (ClassNotFoundException e) {
-            Log.e(TAG, "do you not disable Instant Runt for Android studio?", e);
+            alertHotLoadFailedWarning(context);
+            Log.e(TAG, "hot load failed", e);
             try {
                 aClass = XposedInit.class.getClassLoader().loadClass(Constant.xposedHotloadEntry);
             } catch (ClassNotFoundException e1) {
